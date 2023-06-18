fc657468c636dca79cc1c71a3c55fb8d61fccc65
Qingyun Yang <qyang063@uottawa.ca>
2023-06-17 20:07:13 -0400

    list looks nice

--- react-app/src/components/ListPage.jsx
+++ react-app/src/components/ListPage.jsx
@@ -1,7 +1,7 @@
 import Widget from "./Widget";
 
 export default function ListPage(){
-    return <div>
+    return <div style={{padding:'10px',maxWidth:'1000px'}}>
         <Widget title="1" text="what"/>
         <Widget title="2" text="what?"/>
         <Widget title="3" text="whaaaaaat"/>
--- react-app/src/components/Widget.jsx
+++ react-app/src/components/Widget.jsx
@@ -1,8 +1,31 @@
 export default function Widget(props){
+    const backgroundImageStyle = {
+        backgroundImage: `url("/maxresdefault.jpg")`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '0',
+        paddingTop: '15%', // Adjust this value to set the desired maximum height
+        filter: 'brightness(50%)',
+    };
+    
+      const textStyle = {
+        position: 'absolute',
+        top: '50%',
+        left: '10%',
+        transform: 'translate(-50%, -50%)',
+        color: 'white',
+        textAlign: 'center',
+        
+      };
+      
     return <div className="card" >
-        <img src="/maxresdefault.jpg" className="card-img-top" style={{maxHeight: '200px'}} alt="..."></img>
+        <div style={backgroundImageStyle} alt="...">
+            
+        </div>
+        <div style={textStyle}>
+            <h2>{props.title}</h2>
+            <p>{props.text}</p>
+        </div>
 
-        <h2>{props.title}</h2>
-        <p>{props.text}</p>
     </div>
 }
