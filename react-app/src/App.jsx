b69f972bcdbaabf3e965415d36c270855f02cd70
Qingyun Yang <qyang063@uottawa.ca>
2023-06-21 11:07:41 -0400

    temp save

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,7 +1,28 @@
 import ListPage from "./components/ListPage"
+import {Route,Routes,Link} from 'react-router-dom'
 
 function App() {
-  return <ListPage />
+  return <>
+  <nav>
+    <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/favorite">Favorite</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+  </nav>
+  <Routes>
+    <Route path="/" element={<ListPage/>}/>
+  </Routes>
+  </>
 }
 
 export default App
